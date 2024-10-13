04beb29dbb96ba2c6b84588c2249427b96f7428f
Noval Maulani <novalmaulani17@gmail.com>
2024-10-13 08:27:42 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,77 @@
+// Function to handle button click events
+function selectOption(option) {
+    // Check which option was clicked
+    if (option === 'yes') {
+        // Flash rainbow colors
+        flashRainbowColors(function() {
+            document.getElementById('question').style.display = 'none'; // Hide the question
+            displayCatHeart(); // Display the cat-heart.gif
+            displayInstagramLink(); // Display Instagram link after the cat-heart.gif is shown
+        });
+    } else if (option === 'no') {
+        // Change text on the "No" button to "Follow lah"
+        document.getElementById('no-button').innerText = 'Follow lah'; 
+        // Increase font size of "Yes" button
+        var yesButton = document.getElementById('yes-button');
+        var currentFontSize = window.getComputedStyle(yesButton).getPropertyValue('font-size');
+        var newSize = parseFloat(currentFontSize) * 2; // Increase font size
+        yesButton.style.fontSize = newSize + 'px';
+    } else {
+        // If neither "Yes" nor "No" was clicked, show an alert message
+        alert('Invalid option!');
+    }
+}
+
+// Function to flash rainbow colors and then execute a callback function
+function flashRainbowColors(callback) {
+    var colors = ['#ff0000', '#ff7f00', '#ffff00', '#00ff00', '#0000ff', '#4b0082', '#9400d3'];
+    var i = 0;
+    var interval = setInterval(function() {
+        document.body.style.backgroundColor = colors[i];
+        i = (i + 1) % colors.length;
+    }, 200); // Change color every 200 milliseconds
+    setTimeout(function() {
+        clearInterval(interval);
+        document.body.style.backgroundColor = ''; // Reset background color
+        if (callback) {
+            callback();
+        }
+    }, 2000); // Flash colors for 2 seconds
+}
+
+// Function to display the cat.gif initially
+function displayCat() {
+    var imageContainer = document.getElementById('image-container');
+    var catImage = new Image();
+    catImage.src = 'cat.gif'; // Assuming the cat image is named "cat.gif"
+    catImage.alt = 'Cat';
+    catImage.onload = function() {
+        imageContainer.appendChild(catImage);
+    };
+}
+
+// Function to display the cat-heart.gif
+function displayCatHeart() {
+    document.getElementById('image-container').innerHTML = '';
+    var imageContainer = document.getElementById('image-container');
+    var catHeartImage = new Image();
+    catHeartImage.src = 'cat-heart.gif'; // Assuming the cat-heart image is named "cat-heart.gif"
+    catHeartImage.alt = 'Cat Heart';
+    catHeartImage.onload = function() {
+        imageContainer.appendChild(catHeartImage);
+        document.getElementById('options').style.display = 'none'; // Hide options
+    };
+}
+
+// Function to display the Instagram link
+function displayInstagramLink() {
+    var instagramContainer = document.getElementById('instagram-container');
+    var instagramLink = document.createElement('a');
+    instagramLink.href = 'https://www.instagram.com/ganbala_/profilecard/?igsh=MXIybDV2dndoN3hpeA=='; // Ganti dengan link Instagram Anda
+    instagramLink.target = '_blank';
+    instagramLink.textContent = 'Follow me on Instagram';
+    instagramContainer.appendChild(instagramLink);
+}
+
+// Display the cat.gif initially
+displayCat();
